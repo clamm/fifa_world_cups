4a0f98c19a4ef2e9754bb9dbba4c68c9cb47ba47
clamm <clamm@email.com>
2018-06-16 11:40:53 +0200

    Clean up scores from weird dashes.
    And attempt to get penalty scores as well - which fails because by not iterating games as a block of information we won't know to which game the penalty scores belong to.

--- worldcups/spiders/worldcup2014.py
+++ worldcups/spiders/worldcup2014.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 import scrapy
 
 class Worldcup2014Spider(scrapy.Spider):
@@ -39,12 +40,26 @@ class Worldcup2014Spider(scrapy.Spider):
         else:
             return 1
 
+    def clean_score(self, score_string):
+        # we don't match a dash between the number groups as the scraped dash seems to be different (longer) than the typed dash
+        return re.sub(r'(\d{1,2}).(\d{1,2})(.*)', r'\1-\2', score_string)
+
+    def get_scores(self, raw_scores):
+        # remove the trailing paranthesis in case there was overtime
+        return list(filter(lambda x: x != ')', raw_scores))
+
+    def get_penalities(self, raw_elements):
+        return list(filter(lambda x: re.match(r'.*[()].*', x) is not None, raw_scores)))
 
     def parse(self, response):
-        common_path = '//div[@itemscope="itemscope"]/table/tr[@itemprop="name"]/'
-        home_teams = response.xpath(common_path + 'th[@itemprop="homeTeam"]/span/a/text()').extract()
-        away_teams = response.xpath(common_path + 'th[@itemprop="awayTeam"]/span/span/a/text()').extract()
-        scores_at_ninety_min = response.xpath(common_path + 'th/text()').extract()
+        common_path = '//div[@itemscope="itemscope"]/'
+        teams_and_scores = 'table/tr[@itemprop="name"]/'
+        home_teams = response.xpath(common_path + teams_and_scores + 'th[@itemprop="homeTeam"]/span/a/text()').extract()
+        away_teams = response.xpath(common_path + teams_and_scores + 'th[@itemprop="awayTeam"]/span/span/a/text()').extract()
+        # scores at 90 min or after overtime
+        scores = self.get_scores(response.xpath(common_path + teams_and_scores + 'th/text()').extract())
+
+        penalty_scores = self.get_penalities(response.xpath(common_path + 'table[count(tr)=4]/tr/th/text()').extract())
 
         groups = response.xpath('//h3//span[contains(@id, "Group")]/text()').extract()
         games_per_group = 6
@@ -60,5 +75,6 @@ class Worldcup2014Spider(scrapy.Spider):
                 'group': groups[group_index] if group_index < len(groups) else None,
                 'home_team': home_teams[total_games_idx],
                 'away_team': away_teams[total_games_idx],
-                'score_at_90_min': scores_at_ninety_min[total_games_idx]
+                'score': self.clean_score(scores[total_games_idx])
+
             }
